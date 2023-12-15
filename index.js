15da231d3c1e00c8a0367dca5c97a5e4629842c5
mii9000 <ibrahim.islam@live.com>
2023-12-15 18:58:33 +0600

    last changes

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
-const express = require('express');
 const { Pool } = require('pg');
 const speedTest = require('./speedtest');
-
-const app = express();
-const port = 3000;
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -15,24 +11,10 @@ const pool = new Pool({
 });
 
 const speedtestInterval = parseInt(process.env.SPEEDTEST_INTERVAL);
+console.log(`SpeedTest will run every ${speedtestInterval/60000} minutes`);
 
 setInterval(() => {
     console.log(`[${new Date().toISOString()}] Starting SpeedTest...`)
     speedTest.runSpeedTest(pool);
     console.log(`[${new Date().toISOString()}] SpeedTest Data Saved`)
 }, speedtestInterval);
-
-app.get('/data', async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const result = await client.query('SELECT * FROM speedtest_results ORDER BY timestamp DESC LIMIT 10;');
-        res.json(result.rows);
-    } finally {
-        client.release();
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    console.log(`SpeedTest will run every ${speedtestInterval/60000} minutes`)
-});
